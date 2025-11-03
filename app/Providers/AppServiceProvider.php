c972890de9309acc6900d40f447ef7432baa555b
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2025-11-03 09:26:09 +0700

    feat: Force HTTPS in staging and production environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
+    // if env staging or production, force https
+    if (in_array(config('app.env'), ['staging', 'production'])) {
+      URL::forceScheme('https');
+    }
+
+
     Blade::if('role', function (string $roles, string $mode = 'any') {
       if (!Auth::check()) return false;
       return Auth::user()->matchesRoles($roles, $mode);
